7b1171eb5783cdb6315e4527dd99651b79a2d265
dhirajsinhdeshmukh <drdeshmukh@wpi.edu>
2020-12-01 01:06:57 -0500

    adding Q_learning and montecarlo

---
+++ Frozen_lake_Gym/test.py
@@ -0,0 +1,8 @@
+
+from mdp_dp_test import *
+
+
+if __name__ == "__main__":
+
+    test_render_single()
+
---
+++ GYM_black_jack_and_cliff/MC_on_blackjack_gym/test.py
@@ -0,0 +1,7 @@
+from mc_test import *
+
+
+if __name__ =="__main__":
+    test_mc_control_epsilon_greedy()
+
+    
